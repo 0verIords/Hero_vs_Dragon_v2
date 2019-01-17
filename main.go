0c5673a93380cfd865f5aa0f2902a9c7af1d8bfb
Meder <meder33kg@gmail.com>
2019-01-17 13:10:02 +0600

    #2 Add heroTurn

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 type Persona struct {
 	HP     int
@@ -9,10 +12,8 @@ type Persona struct {
 
 var Hero = Persona{100, 10}
 var Dragon = Persona{100, 25}
-var i int
 
 func GameStatus() {
-	i++
 	fmt.Println("#+++++++++++++++++++++++++++++++")
 	fmt.Println("#Ход: ", i)
 	fmt.Println("#Здоровье Героя: ", Hero.HP)
@@ -20,6 +21,27 @@ func GameStatus() {
 	fmt.Println("#+++++++++++++++++++++++++++++++")
 }
 
-func main() {
+func heroTurn(damage, answer) int {
+	if damage != 0 {
+		Hero.HP -= damage
+	}
 
+	if answer == 1 {
+
+		miss := rand.Intn(100)
+		if miss >= 50 {
+			return Hero.damage
+		} else {
+			return 0
+		}
+	}
+}
+
+func GameRun() {
+
+	GameStatus()
+}
+
+func main() {
+	GameRun()
 }
