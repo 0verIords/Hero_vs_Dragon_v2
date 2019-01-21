974876ecd48f52321422f19fefed84856cf47af5
Meder <meder33kg@gmail.com>
2019-01-21 06:47:53 +0600

    #10 Прошел тест на урон и лечение

--- main_test.go
+++ main_test.go
@@ -4,9 +4,9 @@ import (
 	"testing"
 )
 
-func damageTest(t *testing.T) {
+func TestDamage(t *testing.T) {
 	var in = []int{50, 40, 100, 90}
-	var out = []int{60, 60, 0, 10}
+	var out = []int{50, 60, 0, 10}
 	for i := 0; i < len(out); i++ {
 		hitPoint := 100
 		hitPoint -= hit(in[i])
@@ -17,9 +17,9 @@ func damageTest(t *testing.T) {
 	}
 }
 
-func hillTest(t *testing.T) {
-	var in = []int{100, 55, 15, 90, 35}
-	var out = []int{100, 75, 35, 100, 55}
+func TestHill(t *testing.T) {
+	var in = []int{100, 55, 81, 90, 35, 96}
+	var out = []int{0, 20, 19, 10, 20, 4}
 	for i := 0; i < len(out); i++ {
 		hill := heroHill(in[i])
 		if hill != out[i] {
