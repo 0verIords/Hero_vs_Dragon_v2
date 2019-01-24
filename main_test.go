771d33947b78f2d55e65654439ab77480c41e680
Meder <meder33kg@gmail.com>
2019-01-24 20:13:37 +0600

    changed test func [OLD]-> [NEW]

--- main_test.go
+++ main_test.go
@@ -4,42 +4,40 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDamage(t *testing.T) {
+	assert := assert.New(t)
 	var in = []int{50, 40, 100, 90}
 	var out = []int{50, 60, 0, 10}
 	for i := 0; i < len(out); i++ {
 		hitPoint := 100
 		hitPoint -= hit(in[i])
-		if hitPoint != out[i] {
-			t.Errorf("Неправильный выход! (%v) (%v)", hitPoint, out[i])
-		}
-
+		assert.Equalf(hitPoint, out[i], "Неправильный выход! (%v) (%v)", hitPoint, out[i])
 	}
 }
 
 func TestHill(t *testing.T) {
+	assert := assert.New(t)
 	var in = []int{100, 55, 81, 90, 35, 96}
 	var out = []int{100, 75, 100, 100, 55, 100}
 	for i := 0; i < len(out); i++ {
 		testHero := Hero{in[i], 10}
 		testHero.heroHill()
-		if testHero.HitPoint != out[i] {
-			t.Errorf("Неправильный выход! (%v) != (%v)", testHero.HitPoint, out[i])
-		}
+		assert.Equal(testHero.HitPoint, out[i], "Неправильная работа лечения")
 
 	}
 }
 
 func TestGameStatus(t *testing.T) {
+	assert := assert.New(t)
 	game := Game{15, 55, 44}
 	s := strings.Repeat("#", 15)
 	var in = game.gameStatus()
 	var out = fmt.Sprintf("%s \n#Ход: %v \n#Здоровье Героя: %v \n#Здоровье дракона: %v \n%s", s, 15, 55, 44, s)
-	if in != out {
-		t.Errorf("Неправильная проверка строк")
-	}
+	assert.Equal(in, out, "they should be equal")
 }
 
 func TestMiss(t *testing.T) {
@@ -54,27 +52,25 @@ func TestMiss(t *testing.T) {
 }
 
 func TestChoice(t *testing.T) {
+	assert := assert.New(t)
 	var in = []string{"1", "2", "qweqwe", "sdhsbhsa", "12", "!$@", "sdsda2", "wq"}
 	correct_answer := "#Неправильный ввод!\n#Нужно вводить только 1 или 2"
 	for i := 0; i < len(in); i++ {
 		answer := selectCheck(in[i])
 		if answer != "1" && answer != "2" {
-			if answer != correct_answer {
-				t.Errorf("Не правильная работа выбора")
-			}
+			assert.Equal(answer, correct_answer, "Не правильная работа выбора")
 		}
 	}
 }
 
 func TestCheckWin(t *testing.T) {
+	assert := assert.New(t)
 	var in = []int{100, 0, -20, 10, 30}
 	var out = []bool{true, false, false, true, true}
 	for i := 0; i < len(in); i++ {
 		game := Game{i, in[i], 20}
 		answer := game.gameWin()
-		if answer != out[i] {
-			t.Error("Не правильно работает функция gameWin")
-		}
+		assert.Equal(answer, out[i], "Не правильно работает функция gameWin")
 	}
 }
 
@@ -100,18 +96,16 @@ func TestDragonTurn(t *testing.T) {
 }
 
 func TestGameInput(t *testing.T) {
+	assert := assert.New(t)
 	var in = []string{"1", "0", "2", "1", "2", "4", "5", "6"}
 	for i := 0; i < len(in); i++ {
 		answer := gameInput(in[i])
-		if answer != in[i] {
-			t.Error("Не правильная работа gameInput")
-		}
+		assert.Equal(answer, in[i], "Не правильная работа gameInput")
 	}
 }
 
 func TestGameRun(t *testing.T) {
+	assert := assert.New(t)
 	ans := gameRun(false)
-	if ans != true {
-		t.Error("Не правильная работа gameInput")
-	}
+	assert.Equal(ans, true, "Не правильная работа gameInput")
 }
